05624dd2f5ce0bc102f6cd93bfe5cfebfe8b359e
AyaHassan55 <haya98166@gmail.com>
2026-06-01 23:28:59 +0300

    feat: implement signup page layout and form design 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,26 @@
 
 import './App.css'
 import Header from './components/Header'
+import Signup from './pages/Signup'
 
 function App() {
 
 
   return (
-    <>
+   
       
-      <div >
+      <div className="min-h-screen flex flex-col">
         <Header />
+        <div className="flex-1">
+          <Signup />
+        </div>
       </div>
 
      
 
       
      
-    </>
+    
   )
 }
 
