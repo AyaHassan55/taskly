b569a3728422b2a98e5404f8e5b48a876e2a9411
AyaHassan55 <haya98166@gmail.com>
2026-06-02 02:54:56 +0300

    feat: implement signup page 2

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,45 +1,62 @@
+import iconEmptyCheck from "../assets/icons/icon_empty_check.svg"
+import iconSelect from "../assets/icons/icon_right.svg"
+import eyeShow from "../assets/icons/Icon_show_pass.svg"
 import { useForm, type SubmitHandler } from "react-hook-form";
+
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+    signupSchema,
+    type SignupFormData,
+} from "../schemas/signup.schema";
+import { useState } from "react";
+
 interface IProps {
 
 
+}
 
-}
-type Inputs = {
-    fullName: string
-    email: string
-    jobTitle: string
-    password: string
-    confirmPassword: string
-}
 const Signup = ({ }: IProps) => {
+    const [showPassword, setShowPassword ] = useState(false)
     const { register,
         handleSubmit,
         watch,
+        getValues,
         formState: { errors },
 
-    } = useForm<Inputs>();
-    const password = watch('password');
+    } = useForm<SignupFormData>({
+        resolver: zodResolver(signupSchema),
+        mode: 'onChange',
+    });
+    const password = watch('password', "");
+    const passwordChecks = {
+        length: password.length >= 8,
+        upperLower: /[A-Z]/.test(password) && /[a-z]/.test(password),
 
-    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+        digit: /\d/.test(password),
+        special: /[!@#$%^&*]/.test(password),
+    }
+
+    const onSubmit: SubmitHandler<SignupFormData> = (data) => console.log(data)
     return (
-    <div className='w-full flex justify-center mb-20'>
-        <div className=" w-xl flex flex-col items-center justify-center pb-10 border border-transparent rounded-lg  shadow-[0px_24px_48px_0px_#041B3C0F]  p-[48px]">
-            {/* header form */}
-            <header className=" w-120 h-16 flex-col gap-2 pb-10" >
-                <h1 className="w-120 h-9 text-center text-[30px] font-semibold leading-9 tracking-[-0.75px]">Create your workspace</h1>
-                <p className="w-120 h-5 text-center text-[14px] font-normal leading-5 tracking-normal text-slate-700">
-                    Join the editorial approach to task management.</p>
+        <div className='w-full flex justify-center mb-20'>
+            <div className=" w-xl flex flex-col items-center justify-center pb-10 border border-transparent rounded-lg  shadow-[0px_24px_48px_0px_#041B3C0F]  p-12">
+                {/* header form */}
+                <header className=" w-120 h-16 flex-col gap-2 pb-10" >
+                    <h1 className="w-120 h-9 text-center text-[30px] font-semibold leading-9 tracking-[-0.75px]">Create your workspace</h1>
+                    <p className="w-120 h-5 text-center text-[14px] font-normal leading-5 tracking-normal text-slate-700">
+                        Join the editorial approach to task management.</p>
 
-            </header>
+                </header>
 
-            {/* main form */}
-            <main>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    {/* name input */}
-                    <div className="flex flex-col gap-2 w-120 h-23.25">
-                        <label htmlFor="fullName" className='text-slate-700 font-bold text-[11px] tracking-[0.55px] leading-[16.5px] uppercase '>Full Name</label>
+                {/* main form */}
+                <main>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        {/* name input */}
+                        <div className="flex flex-col gap-2 w-120 h-23.25">
+                            <label htmlFor="fullName" className='text-slate-700 font-bold text-[11px] tracking-[0.55px] leading-[16.5px] uppercase '>Full Name</label>
 
-                        <input className="w-120 h-12 border border-transparent rounded-sm py-3.5 px-4 bg-(--color-surface-highest)
+                            <input className="w-120 h-12 border border-transparent rounded-sm  focus:border-blue-500 py-3.5 px-4 bg-(--color-surface-highest)
                         placeholder:text-[#737685] 
                         placeholder:text-[16px] 
                         placeholder:leading-[100%] 
@@ -48,18 +65,17 @@ const Signup = ({ }: IProps) => {
                         
                         
                         "
-                            placeholder="Enter your full name" type="text"
-                            {...register("fullName", {
-                                required: "3-50 characters, letters only.",
-                            })}
-                        />
-                    </div>
-                    {errors.fullName && <p className="text-[#C3C6D6] text-[11px] leading-[16.5px] tracking-normal">{errors.fullName.message}</p>}
-                    {/* email input */}
-                    <div className="flex flex-col gap-2 w-120 h-23.25">
-                        <label htmlFor="email" className='text-slate-700 font-bold text-[11px] tracking-[0.55px] leading-[16.5px] uppercase '>Email</label>
 
-                        <input className="w-120 h-12 border border-transparent rounded-sm py-3.5 px-4 bg-(--color-surface-highest)
+                                placeholder="Enter your full name" type="text"
+                                {...register("fullName")}
+                            />
+                        </div> 
+                        {errors.fullName && <p className="text-[#C3C6D6] text-[11px] leading-[16.5px] tracking-normal">{errors.fullName.message}</p>}
+                        {/* email input */}
+                        <div className="flex flex-col gap-2 w-120 h-23.25">
+                            <label htmlFor="email" className='text-slate-700 font-bold text-[11px] tracking-[0.55px] leading-[16.5px] uppercase '>Email</label>
+
+                            <input className="w-120 h-12 border border-transparent rounded-sm py-3.5 px-4 bg-(--color-surface-highest)
                         placeholder:text-[#737685] 
                         placeholder:text-[16px] 
                         placeholder:leading-[100%] 
@@ -68,25 +84,20 @@ const Signup = ({ }: IProps) => {
                         
                         
                         "
-                            placeholder="yourname@company.com" type="email"
-                            {...register("email", {
-                                required: "Please enter a valid email address.",
-                                pattern: {
-                                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                                    message: "Please enter a valid email address."
-                                }
-                            })}
-                        />
-                    </div>
-                    {errors.email && <p className="text-[#C3C6D6] text-[11px] leading-[16.5px] tracking-normal">{errors.email.message}</p>}
+                                placeholder="yourname@company.com" type="email"
+                                {...register("email")}
 
-                    {/* Job title */}
-                    <div className="flex flex-col gap-2 w-120 h-23.25">
-                        <label htmlFor="jobTitle" className='text-slate-700 font-bold 
+                            />
+                        </div>
+                        {errors.email && <p className="text-[#C3C6D6] text-[11px] leading-[16.5px] tracking-normal">{errors.email.message}</p>}
+
+                        {/* Job title */}
+                        <div className="flex flex-col gap-2 w-120 h-23.25">
+                            <label htmlFor="jobTitle" className='text-slate-700 font-bold 
                         text-[11px] tracking-[0.55px] leading-[16.5px] uppercase '
-                        >Job Title <span className="font-[400px] text-[11px] leading-[100%] tracking-normal lowercase text-[#737685] ">(optional)</span></label>
+                            >Job Title <span className="font-[400px] text-[11px] leading-[100%] tracking-normal lowercase text-[#737685] ">(optional)</span></label>
 
-                        <input className="w-120 h-12 border border-transparent rounded-sm py-3.5 px-4 bg-(--color-surface-highest)
+                            <input className="w-120 h-12 border border-transparent rounded-sm py-3.5 px-4 bg-(--color-surface-highest)
                         placeholder:text-[#737685] 
                         placeholder:text-[16px] 
                         placeholder:leading-[100%] 
@@ -95,74 +106,127 @@ const Signup = ({ }: IProps) => {
                         
                         
                         "
-                            placeholder="e.g. Project Manager"
-                            {...register("jobTitle")}
-                        />
-                    </div>
-
-                    {/* password */}
-                    <div className='grid grid-cols-2 gap-4 w-120 '>
-                        <div className="flex flex-col gap-2 w-120 h-23.25">
-                            <label htmlFor="password" className='text-slate-700 font-bold text-[11px] tracking-[0.55px] leading-[16.5px] uppercase '>Password</label>
-
-                            <input className="w-58 h-12 border border-transparent rounded-sm py-3.5 px-4 bg-(--color-surface-highest)
-                        placeholder:text-[#737685] 
-                        placeholder:text-[16px] 
-                        placeholder:leading-[100%] 
-                        placeholder:tracking-normal 
-                        placeholder:font-normal
-                        
-                        
-                        "
-                                placeholder="Password"
-                                type="password"
-                                {...register("password", {
-                                    required: "Password is required.",
-                                    minLength: {
-                                        value: 8,
-                                        message: "Password must be at least 6 characters long."
-                                    }
-                                })}
-                            />
-                            {errors.password && <p>{errors.password.message}</p>}
-
-                        </div>
-                       
-                        {/* --------confirm password-------------- */}
-                        <div className="flex flex-col gap-2 w-120 h-23.25">
-                            <label htmlFor="confirmPassword" className='text-slate-700 font-bold text-[11px] tracking-[0.55px] leading-[16.5px] uppercase '>Confirm Password</label>
-
-                            <input className="w-58 h-12 border border-transparent rounded-sm py-3.5 px-4 bg-(--color-surface-highest)
-                        placeholder:text-[#737685] 
-                        placeholder:text-[16px] 
-                        placeholder:leading-[100%] 
-                        placeholder:tracking-normal 
-                        placeholder:font-normal
-                        
-                        
-                        "
-                                placeholder="Repeat password" type="password"
-                                {...register("confirmPassword", {
-                                    required: "Confirm your password",
-
-                                    validate: (value) =>
-                                        value === password ||
-                                        "Passwords do not match",
-                                })}
+                                placeholder="e.g. Project Manager"
+                                {...register("jobTitle")}
                             />
                         </div>
-                    </div>
 
-                    {/* submit button */}
-                    <button className="w-120 h-12 cursor-pointer bg-linear-to-r from-[#0052CC] to-[#003D9B] rounded-sm text-white font-bold text-[16px] leading-[100%] tracking-normal mt-6 shadow-[0_1px_2px_0_rgba(0,0,0,0.05)]" type="submit">Create Account</button>
-                </form>
-            </main>
-            {/* footer form */}
-            <footer className="mt-6 flex justify-center">
-                <p className="flex items-center gap-1 text-center text-[14px] font-normal leading-5 tracking-normal text-slate-700 ">Already have an account? <a href="#" className="text-(--color-primary) font-semibold "> Log in</a></p>
-            </footer>
+                        {/* password */}
+                        <div className='grid grid-cols-2 gap-4 w-120 '>
+                            <div className="flex flex-col gap-2 w-full h-23.25 relative">
+                                <label htmlFor="password" className='text-slate-700 font-bold text-[11px] tracking-[0.55px] leading-[16.5px] uppercase '>Password</label>
+
+                                <input className="w-58 h-12 border border-transparent rounded-sm py-3.5 px-4 bg-(--color-surface-highest)
+                        placeholder:text-[#737685] 
+                        placeholder:text-[16px] 
+                        placeholder:leading-[100%] 
+                        placeholder:tracking-normal 
+                        placeholder:font-normal
+                        
+                        
+                        "
+                                    placeholder="Password"
+                                    type= {showPassword ? "text":"password"}
+                                    {...register("password")}
+
+                                />
+                                <img
+                                    src={eyeShow}
+                                    alt="toggle password"
+                                    onClick={() => setShowPassword(!showPassword)}
+                                    className="absolute left-50 top-1/2 -translate-y-1/2 cursor-pointer w-5 h-5"
+                                   
+                                />
+                                {errors.password && <p>{errors.password.message}</p>}
+
+                            </div>
+
+                            {/* --------confirm password-------------- */}
+                            <div className="flex flex-col gap-2 w-full h-23.25">
+                                <label htmlFor="confirmPassword" className='text-slate-700 font-bold text-[11px] tracking-[0.55px] leading-[16.5px] uppercase '>Confirm Password</label>
+
+                                <input className="w-58 h-12 border border-transparent rounded-sm py-3.5 px-4 bg-(--color-surface-highest)
+                                      placeholder:text-[#737685] 
+                                        placeholder:text-[16px] 
+                                        placeholder:leading-[100%] 
+                                        placeholder:tracking-normal 
+                                        placeholder:font-normal
+                        
+                        
+                        "
+                                    placeholder="Repeat password" type="password"
+                                    {...register("confirmPassword", {
+                                        required: "Confirm your password",
+
+                                        validate: (value) =>
+                                            value === getValues("password") ||
+                                            "Passwords do not match",
+                                    })}
+                                />
+                            </div>
+                        </div>
+                        <div className="mt-2 bg-[#E8EDFF] rounded-lg p-4">
+                            <div className="flex items-center gap-2">
+                                <span>
+                                    <img
+                                        src={
+                                            passwordChecks.length
+                                                ? iconSelect
+                                                : iconEmptyCheck
+                                        }
+                                        alt="check icon"
+                                    />
+                                </span>
+
+                                <p className="text-[11px] text-[#434654] ">
+                                    At least 8 characters
+                                </p>
+                            </div>
+
+                            <div className="flex items-center gap-2 mt-2">
+                                <span>
+                                    <img
+                                        src={
+                                            passwordChecks.upperLower && passwordChecks.digit
+                                                ? iconSelect
+                                                : iconEmptyCheck
+                                        }
+                                        alt="check icon"
+                                    />
+                                </span>
+
+                                <p className="text-[11px] text-[#434654] ">
+                                    One uppercase, lowercase and
+                                    digit
+                                </p>
+                            </div>
+
+                            <div className="flex items-center gap-2 mt-2">
+                                <span>
+                                    <img
+                                        src={
+                                            passwordChecks.special ? iconSelect : iconEmptyCheck
+                                        }
+                                        alt="check icon"
+                                    />
+                                </span>
+
+                                <p className="text-[11px] text-[#434654] ">
+                                    One special character
+                                </p>
+                            </div>
+                        </div>
+
+                        {/* submit button */}
+                        <button className="w-120 h-12 cursor-pointer bg-linear-to-r from-[#0052CC] to-[#003D9B] rounded-sm text-white font-bold text-[16px] leading-[100%] tracking-normal mt-6 shadow-[0_1px_2px_0_rgba(0,0,0,0.05)]" type="submit">Create Account</button>
+                    </form>
+                </main>
+                {/* footer form */}
+                <footer className="mt-6 flex justify-center">
+                    <p className="flex items-center gap-1 text-center text-[14px] font-normal leading-5 tracking-normal text-slate-700 ">Already have an account? <a href="#" className="text-(--color-primary) font-semibold "> Log in</a></p>
+                </footer>
+            </div>
         </div>
-</div>
 
     )
 }
---
+++ src/schemas/signup.schema.ts
@@ -0,0 +1,44 @@
+
+
+import { z } from "zod";
+
+const nameRegex =
+  /^(?!.*\s{2,})[\p{L}]+(?: [\p{L}]+)*$/u;
+
+const passwordRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[^\s]{8,64}$/;
+
+export const signupSchema = z
+  .object({
+    fullName: z
+      .string()
+      .trim()
+      .min(3, "Name must be at least 3 characters")
+      .max(50, "Name must be at most 50 characters")
+      .regex(
+        nameRegex,
+        "Only letters and single spaces are allowed"
+      ),
+
+    email: z
+      .string()
+      .trim()
+      .email("Please enter a valid email address"),
+
+    jobTitle: z.string().optional(),
+
+   password:z.string().min(1, "Password is required"),
+
+    confirmPassword: z.string(),
+  })
+  .refine(
+    (data) => data.password === data.confirmPassword,
+    {
+      path: ["confirmPassword"],
+      message: "Passwords do not match",
+    }
+  );
+
+export type SignupFormData = z.infer<
+  typeof signupSchema
+>;
